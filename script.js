4f025bcd6e3115fde12b9b5291687289a6437a42
Macnelson9 <michaelofatu@gmail.com>
2025-05-04 01:25:35 +0100

    Basic setup and file imports

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 'use script';
+
+// Initialize lenis
+const lenis = new Lenis();
+
+lenis.on('scroll', ScrollTrigger.update);
+
+gsap.ticker.add((time) => {
+    lenis.raf(time * 1000)
+});
+
+gsap.ticker.lagSmoothing(0);
